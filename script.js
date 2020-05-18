1ad481982a2d48d044d2dbd870a4e2314711b9f1
kericson1 <59373391+kericson1@users.noreply.github.com>
2020-05-18 12:40:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 (function() {
-
     var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-    // See Lecture 50, part 1
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
